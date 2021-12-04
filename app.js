c138a676e02a0c0638d1a698ddde3dd522a371b0
debojit-basak <39153154+debojit-basak@users.noreply.github.com>
2021-12-04 23:21:07 +0600

    simple todo list with crud and node - started the basics

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send("Task manager App")
+})
+
+const port = 3000;
+app.listen(port, console.log(`port ${port}`));
