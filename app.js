508b65cc07938e4ea77a42a0537ab6d8af8d4034
debojit-basak <39153154+debojit-basak@users.noreply.github.com>
2021-12-05 09:56:04 +0600

    facing problem with middleware

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
 
+
+app.use(express.json())
 //routes
 app.get('/hello', (req, res) => {
     res.send("Task manager App")
 })
+app.use('/api/v1/tasks', tasks)
+
+//routs
+//app.get('/api/v1/tasks') - get all the tasks
+//app.post('/api/v1/tasks') - create a new task
+//app.get('/api/v1/tasks/:id') - get a single task
+//app.patch('/api/v1/tasks/:id) - update task
+//app.delete('/api/v1/tasks/:id') - delete task
 
 const port = 3000;
 app.listen(port, console.log(`port ${port}`));
